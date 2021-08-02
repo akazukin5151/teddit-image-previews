7411967bc6fee4722436b3023219cb1c367455b6
twenty5151 <tsuiyikching@protonmail.com>
2021-08-02 22:21:38 +0100

    Restore original image height as well; scroll preview into view

--- src/teddit-image-previews.ts
+++ src/teddit-image-previews.ts
@@ -3,6 +3,7 @@ const pictures: Element[] = Array.from(gallery.children)
 const smallUrls: string[] = pictures.map(e => elementToImg(e).src)
 const bigUrls: string[] = pictures.map(getHref)
 let currentImage = 0
+let deliberatelyToggled = false
 
 function getHref(elem: Element): string {
     let anchor = <HTMLAnchorElement>elem.children[1]
@@ -20,17 +21,24 @@ function swap(a: Element, b: Element) {
 function enlargeImage(image: Element, shouldSwap: boolean) {
     const img = elementToImg(image)
     img.src = bigUrls[currentImage]
+    // TODO: doesn't this mess up the aspect ratio?
     // gallery has margin-left of 60px
     img.width = document.documentElement.clientWidth - 60
+    img.height = document.documentElement.clientHeight
     if (shouldSwap) {
         swap(gallery.children[currentImage], gallery.children[0])
+    }
+    if (deliberatelyToggled) {
+        img.scrollIntoView()
     }
 }
 
 function restoreCurrentImage() {
     const img = elementToImg(pictures[currentImage])
     img.src = smallUrls[currentImage]
+    // defaults
     img.width = 108
+    img.height = 61
     // Swap with all previous images in increasing order
     for (let i = 0; i !== currentImage; i++) {
         swap(gallery.children[currentImage], gallery.children[i])
@@ -52,8 +60,10 @@ function previousImage() {
 
 document.addEventListener('keypress', function onPress(event) {
     if (event.key === 'a' && currentImage > 0) {
+        deliberatelyToggled = true
         return previousImage()
     } else if (event.key === 'd' && currentImage < pictures.length - 1) {
+        deliberatelyToggled = true
         return nextImage()
     }
 })
