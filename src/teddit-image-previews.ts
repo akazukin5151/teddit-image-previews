cf75352a714c4579fb80c4e8d35c074de30a6bdf
twenty5151 <tsuiyikching@protonmail.com>
2021-08-02 23:45:54 +0100

    Add prev and next buttons as indicator of multiple images

--- src/teddit-image-previews.ts
+++ src/teddit-image-previews.ts
@@ -3,7 +3,7 @@ const pictures: Element[] = Array.from(gallery.children)
 const smallUrls: string[] = pictures.map(e => elementToImg(e).src)
 const bigUrls: string[] = pictures.map(getHref)
 let currentImage = 0
-let deliberatelyToggled = false
+
 
 function getHref(elem: Element): string {
     let anchor = <HTMLAnchorElement>elem.children[1]
@@ -18,7 +18,7 @@ function swap(a: Element, b: Element) {
     gallery.insertBefore(a, b)
 }
 
-function enlargeImage(image: Element, shouldSwap: boolean) {
+function enlargeImage(image: Element, shouldSwap: boolean, deliberatelyToggled: boolean) {
     const img = elementToImg(image)
     img.src = bigUrls[currentImage]
     // TODO: doesn't this mess up the aspect ratio?
@@ -45,27 +45,58 @@ function restoreCurrentImage() {
     }
 }
 
-function nextImage() {
-    restoreCurrentImage()
-    currentImage++
-    enlargeImage(pictures[currentImage], true)
-}
-
-function previousImage() {
-    restoreCurrentImage()
-    currentImage--
-    enlargeImage(pictures[currentImage], false)
-}
-
-
-document.addEventListener('keypress', function onPress(event) {
-    if (event.key === 'a' && currentImage > 0) {
-        deliberatelyToggled = true
-        return previousImage()
-    } else if (event.key === 'd' && currentImage < pictures.length - 1) {
-        deliberatelyToggled = true
-        return nextImage()
+function nextImage(deliberatelyToggled: boolean) {
+    if (currentImage < pictures.length - 1) {
+        restoreCurrentImage()
+        currentImage++
+        enlargeImage(pictures[currentImage], true, deliberatelyToggled)
     }
-})
+}
 
-enlargeImage(pictures[0], true)
+function previousImage(deliberatelyToggled: boolean) {
+    if (currentImage > 0) {
+        restoreCurrentImage()
+        currentImage--
+        enlargeImage(pictures[currentImage], false, deliberatelyToggled)
+    }
+}
+
+function make_button(label: string, onclick: () => void) {
+    let btn = document.createElement("button")
+    btn.innerHTML = label
+    btn.style.background = 'none'
+    btn.style.border = 'none'
+    btn.style.fontSize = 'small'
+    btn.style.color = '#0645ad'
+    btn.style.cursor = 'pointer'
+    btn.onclick = onclick
+    return btn
+}
+
+function add_buttons() {
+    const info = document.querySelectorAll('.info')[0]
+    const title = info.children[1]
+    const links = title.children[4]
+
+    let prev = make_button('prev', () => previousImage(true))
+    prev.style.marginLeft = '10px'
+    prev.style.marginRight = '10px'
+    links.append(prev)
+
+    let next = make_button('next', () => nextImage(true))
+    links.append(next)
+}
+
+function main() {
+    document.addEventListener('keypress', (event) => {
+        if (event.key === 'a') {
+            return previousImage(true)
+        } else if (event.key === 'd') {
+            return nextImage(true)
+        }
+    })
+    enlargeImage(pictures[0], true, false)
+    add_buttons()
+}
+
+main()
